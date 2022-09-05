6b914119813fb564e51b88a196c9259511800883
igor <kishm2013@mail.ru>
2022-09-05 13:50:31 +0300

    итоговая задача первой четверти

---
+++ TZ/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+string[] array = {"qwer","4tyu","h","wrvkr","hj"};
+string[] new_array = new string[array.Length];
+
+void PrintArray(string[] array )
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            Console.Write($"{array[i]} ");
+        }
+       
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if( array[i].Length <= 3  ) 
+    {
+        new_array[i] = new_array[i] + array[i];
+    }
+    
+}
+PrintArray(new_array);
